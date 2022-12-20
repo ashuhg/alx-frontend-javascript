669c4565eeef464ed47aa4456cf7f286a93bb754
ashuhg <ashenafihagos.ah@gmail.com>
2022-12-20 04:07:40 +0300

    es6 basics

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const employees = [];
+  for (const employee of Object.values(report.allEmployees)) {
+    employees.push(...employee);
+  }
+
+  return employees;
+}
